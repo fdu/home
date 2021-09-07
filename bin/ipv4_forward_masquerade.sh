dc756b8945a951c432f085ac3e8e4dc517db698e
fdu <1050329+fdu@users.noreply.github.com>
2021-09-07 21:34:12 +0200

    Update ipv4_forward_masquerade.sh

--- bin/ipv4_forward_masquerade.sh
+++ bin/ipv4_forward_masquerade.sh
@@ -2,4 +2,5 @@
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -P FORWARD ACCEPT
 #iptables -t nat -A POSTROUTING -s 192.168.8.1/24 -o wlp2s0 -j MASQUERADE
